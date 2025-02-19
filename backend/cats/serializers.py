cecbedb1e29ff7d3cb679d7718ada50fe9ab6f91
Sugakov Denis <Sugakovdv@yandex.ru>
2025-02-19 17:03:35 +0300

    fix lint

--- backend/cats/serializers.py
+++ backend/cats/serializers.py
@@ -63,33 +63,39 @@ class CatSerializer(serializers.ModelSerializer):
         return dt.datetime.now().year - obj.birth_year
 
     def create(self, validated_data):
-        if "achievements" not in self.initial_data:
+        if 'achievements' not in self.initial_data:
             cat = Cat.objects.create(**validated_data)
             return cat
-        else:
-            achievements = validated_data.pop("achievements")
-            cat = Cat.objects.create(**validated_data)
-            for achievement in achievements:
-                current_achievement, status = Achievement.objects.get_or_create(
-                    **achievement
-                )
-                AchievementCat.objects.create(achievement=current_achievement, cat=cat)
-            return cat
-
+        achievements = validated_data.pop('achievements')
+        cat = Cat.objects.create(**validated_data)
+        for achievement in achievements:
+            current_achievement, status = Achievement.objects.get_or_create(
+            **achievement
+            )
+            AchievementCat.objects.create(
+            achievement=current_achievement, cat=cat
+            )
+        return cat
     def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.color = validated_data.get("color", instance.color)
-        instance.birth_year = validated_data.get("birth_year", instance.birth_year)
-        instance.image = validated_data.get("image", instance.image)
-        if "achievements" in validated_data:
-            achievements_data = validated_data.pop("achievements")
-            lst = []
-            for achievement in achievements_data:
-                current_achievement, status = Achievement.objects.get_or_create(
-                    **achievement
-                )
-                lst.append(current_achievement)
-            instance.achievements.set(lst)
+        instance.name = validated_data.get('name', instance.name)
+        instance.color = validated_data.get('color', instance.color)
+        instance.birth_year = validated_data.get(
+        'birth_year', instance.birth_year
+        )
+        instance.image = validated_data.get('image', instance.image)
+
+        if 'achievements' not in validated_data:
+            instance.save()
+            return instance
+
+        achievements_data = validated_data.pop('achievements')
+        lst = []
+        for achievement in achievements_data:
+            current_achievement, status = Achievement.objects.get_or_create(
+            **achievement
+            )
+            lst.append(current_achievement)
+        instance.achievements.set(lst)
 
         instance.save()
         return instance
