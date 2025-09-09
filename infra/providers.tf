8deecc0f1ee4679331f3d8b039f7a723f9f6a3e7
Sugakov Denis <Sugakovdv@yandex.ru>
2025-09-09 14:33:40 +0300

    upd secrets

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,26 +1,31 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "~> 0.84"
+      version = "~> 0.109"
     }
   }
-  required_version = ">= 1.0"
 
   backend "s3" {
-    endpoint = "https://storage.yandexcloud.net"
-    bucket = "kittygram-terraform-state-158160191213"
-    region = "ru-central1"
-    key    = "tf-state.tfstate"
+    endpoints = { s3 = "https://storage.yandexcloud.net" }
+    bucket    = "kittygram-tf-state-gorban"
+    region    = "ru-central1"
+    key       = "tf-state.tfstate"
 
     skip_region_validation      = true
     skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
   }
 }
 
 provider "yandex" {
-  service_account_key_file = var.service_account_key_file
+  service_account_key_file = var.sa_key_file
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
-  zone                     = var.default_zone
+  zone                     = var.zone
 }
